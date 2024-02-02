08975f9ddef1b56ca6e297887902b7413919a913
Soo Hwan Na <whiteshell2544@naver.com>
2024-02-02 11:33:24 +0900

    baffinvektt: Initial TWRP device tree
    Made with SebaUbuntu's TWRP device tree generator
    Arch: arm
    Manufacturer: samsung
    Model: SHV-E275K
    Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_baffinvektt.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_baffinvektt-user \
+    omni_baffinvektt-userdebug \
+    omni_baffinvektt-eng
---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_baffinvektt-user
+add_lunch_combo omni_baffinvektt-userdebug
+add_lunch_combo omni_baffinvektt-eng
